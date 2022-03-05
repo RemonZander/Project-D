4615e2eeb508b8c4aa87c77b4ea4e0dd23e9e394
NielsPoldervaart <Niels@meghira.nl>
2022-03-05 20:12:29 +0100

    Added POST request with image object. Works?? I think.

--- WebExtension/searchImage.js
+++ WebExtension/searchImage.js
@@ -5,15 +5,44 @@ chrome.contextMenus.create({
     contexts: ["image"]
 });
 
-/* Register a listener for the `onClicked` event */
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (tab && info.mediaType === "image") {
-        /* Create the code to be injected */
-        var code = [
-            'window.location.href = "https://www.bol.com";',
-        ].join("\n");
 
-        /* Inject the code into the current tab */
-        chrome.tabs.executeScript(tab.id, { code: code });
+/* Register a listener for the `onClicked` event */
+chrome.contextMenus.onClicked.addListener((clickedData, tab) => {
+    if (tab && clickedData.mediaType === "image" && clickedData.menuItemId === "bolImageSearch") {
+
+        const data = new FormData();
+        data.append("image", clickedData.srcUrl);
+
+        chrome.runtime.sendMessage(
+            {
+                query: "POST",
+                data: data,
+                url: "http://127.0.0.1:5000/image"
+            }, 
+            (res) => {
+                if (res !== undefined && res !== "") {
+                    console.log(res);
+                }
+                else {
+                    console.log(`empty res: ${res}`);
+                }
+            }
+        );
+    }
+});
+
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => { 
+    if (request.query == "POST") {
+        fetch(request.url, {
+            method: 'POST',
+            body: request.data
+        })
+        .then(res => res.json())
+        .then(res => sendResponse(res))
+        .catch(err => console.error(`Error: ${err}`));
+
+        return true
     }
 });
