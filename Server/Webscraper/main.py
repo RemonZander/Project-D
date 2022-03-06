d6715b725bc7edf5e331f8a8e49e970879c112ef
Sebastiaan Booman <iMeowTwitch@gmail.com>
2022-03-06 18:23:52 +0100

    Added basic recursion loop, lowest sub-categories

--- Server/Webscraper/main.py
+++ Server/Webscraper/main.py
@@ -45,15 +45,42 @@ def get_one():
     print(sub_category_url)
 
     driver.get(sub_category_url)
-    with open ("scraped_book.png", "wb") as file:
-        l = driver.find_element(By.CSS_SELECTOR, ".js_item_root:not(.js_sponsored_product) > div > div > a > img").get_attribute("src")
+    #with open ("scraped_book.png", "wb") as file:
+        #l = driver.find_element(By.CSS_SELECTOR, ".js_item_root:not(.js_sponsored_product) > div > div > a > img").get_attribute("src")
         #.product-image.product-image--list.px_list_page_product_click
         #".product-image.product-image--list.px_list_page_product_click > img"
-        r = requests.get(l, allow_redirects=True)
+        #r = requests.get(l, allow_redirects=True)
 
-        file.write(r.content)
-        #file.write(l.)
+        #file.write(r.content)
+
+    test = driver.find_element(By.CSS_SELECTOR, ".facet-control__filter.facet-control__filter--no-padding > a").get_attribute("href")
+    print(test)
     #driver.quit()
+
+def get_categories(url):
+    driver.get(url)
+
+def find_lowest_categories(start_url):
+    #Locate elements
+    driver.get(start_url)
+    sub_categories = driver.find_elements(By.CSS_SELECTOR, ".facet-control__filter.facet-control__filter--no-padding > a")
+    #If elements that indicate more sub-categories cant be found, return append url to lowest_categories_list
+    #print("type" + str(type(sub_categories)))
+    if len(sub_categories) == 0:
+        #TODO: Add category URL to list
+        print("url:" + start_url)
+        #driver.implicitly_wait(3)
+        return
+
+    #Extract hrefs from categories
+    extracted_url_list = []
+    for sub_cat in sub_categories:
+        extracted_url_list.append(sub_cat.get_attribute("href")) 
+
+    #Else call function with next sub-category url
+        #For each sub-category -> find_lowest_categories(sub_category.url)
+    for url in extracted_url_list:
+        find_lowest_categories(url)
 
 
 if __name__ == '__main__':
@@ -61,11 +88,18 @@ if __name__ == '__main__':
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    driver.implicitly_wait(15)
+    #driver.implicitly_wait(5)
     driver.maximize_window()
     driver.get(base_url)
 
     #ACCEPT COOKIES
     driver.find_element(By.CSS_SELECTOR, "wsp-consent-modal > div > button").click()
-    get_list()
+    #get_list()
     #get_one()
+    print("yeet")
+    find_lowest_categories("https://www.bol.com/nl/nl/l/boeken/8299/")
+
+    #lowest_categories_list = []
+
+#NOTES:
+#MULTIPLE OF SAME CATEGORIES ARE FOUND. EG: "Boeken" has sub category "ALL" containing all books
