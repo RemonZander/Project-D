2aab108d149a41166d1a739027bd96b35b4d09ad
NielsPoldervaart <Niels@meghira.nl>
2022-03-17 15:35:11 +0100

    Basic multithreading implementation

--- Server/Python server/Python_server.py
+++ Server/Python server/Python_server.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import concurrent.futures
+import time
 
 app = Flask(__name__)
 
@@ -21,6 +23,29 @@ def check_file_type(filename):
         if filename.endswith(type):
             return True
     return False
+
+def run_test(secs):
+    print(f'Sleeping {secs} second(s)...')
+    time.sleep(secs)
+    return f'Done sleeping for {secs} second(s)...'
+
+def threading_test():
+    start = time.perf_counter()
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # sec_list = [5,4,3,2,1]
+        # results = [executor.submit(run_test, sec) for sec in sec_list]
+        results = [executor.submit(run_test, 1) for _ in range(10)]
+
+        for r in concurrent.futures.as_completed(results):
+            print(r.result())
+
+    finish = time.perf_counter()
+
+    print(f'Finished in {round(finish - start, 2)} second(s)...')
+
+
+threading_test()
 
 if __name__ == '__main__':
     app.run()
