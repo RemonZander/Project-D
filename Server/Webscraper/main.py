a832129c00e6e8b355eb34a874463c53a5f4db5b
SebastiaanBooman <iMeowTwitch@gmail.com>
2022-03-19 20:49:39 +0100

    updated scraper. partial implementation edge case

--- Server/Webscraper/main.py
+++ Server/Webscraper/main.py
@@ -83,10 +83,11 @@ def find_lowest_categories(start_url, path=""):
 
         #GET LIST OF ALL ITEMS
         #SET MAXIMUM OF PAGES TO LOAD (EACH PAGE HAS MAXIMUM OF 24 PRODUCTS)
-        for n in range(5):
-            #TRY INCASE PAGE DOES NOT EXIST (OUT OF RANGE)
+        
+        for n in range(1,5):
+            #TRY INCASE PAGE DOES NOT EXIST (OUT OF RANGE), OR PAGE SETUP IS DIFFERENT (CLOTHING FOR EXAMPLE HAS DIFFERENT SETUP)
             try:
-                n = n +1
+                #n = n +1
                 items = driver.find_elements(By.CSS_SELECTOR, ".js_item_root:not(.js_sponsored_product)")
                 print(f"length: {len(items)}")
                 print(f"page: {n}")
@@ -104,29 +105,41 @@ def find_lowest_categories(start_url, path=""):
                     print(name, image_link, product_link)
                     
             except:
-                break
-            driver.get(start_url + f"?page={n+1}")
+                print("ERROR!")
+                #TRY INCASE PAGE DOES NOT EXIST (OUT OF RANGE)
+                try:
+                items = driver.find_elements(By.CSS_SELECTOR, ".js_item_root:not(.js_sponsored_product)")
+                    print(f"length: {len(items)}")
+                    print(f"page: {n}")
+                    for item in items:
+                        #RETRIEVE NAME
+                        name = item.find_element(By.CSS_SELECTOR, ".product-item__content > a > span").text
+                        print(name)
+                except:
+                    print("NOPE")
+                    break
+            driver.get(start_url + f"?page={n}")
         return
 
     #Extract hrefs from categories
     extracted_url_list = []
 
     ##FOR LIMITED CATEGORIES RANGE
-    limit = 1
-    for i in range(limit):
-        try: #We arent checking the range of the actual list, so if its shorter than the limit we just break out of the loop
-            extracted_url_list.append(sub_categories[i].get_attribute("href"))
-        except:
-            break
+    #limit = 1
+    #for i in range(limit):
+        #try: #We arent checking the range of the actual list, so if its shorter than the limit we just break out of the loop
+            #extracted_url_list.append(sub_categories[i].get_attribute("href"))
+       # except:
+            #break
 
     ##ALL CATEGORIES
-    #for sub_cat in sub_categories:
-        #extracted_url_list.append(sub_cat.get_attribute("href"))
+    for sub_cat in sub_categories:
+        extracted_url_list.append(sub_cat.get_attribute("href"))
 
     #Else call function with next sub-category url
         #For each sub-category -> find_lowest_categories(sub_category.url)
     for url in extracted_url_list:
-        find_lowest_categories(url, start_url + path) #TODO: ADD DELIMER FOR PATH HIERARCHY
+        find_lowest_categories(url, f"{start_url}>{path}") #Delimeter is ">"
 
 
 if __name__ == '__main__':
@@ -143,22 +156,22 @@ if __name__ == '__main__':
     #get_list()
     #get_one()
     #print("GETTING ALL SUB-CATEGORY URLS")
-    find_lowest_categories("https://www.bol.com/nl/nl/l/boeken/8299/")
+    #find_lowest_categories("https://www.bol.com/nl/nl/l/boeken/8299/")
 
 
 
     #MAIN-CATEGORIES WE WANT TO SCRAPE (HARD CODED TO EXLUDE SPECIFIC CATEGORIES)
     categories_urls = [
-        "https://www.bol.com/nl/nl/menu/categories/subMenu/3",  #Computer en Elekronica
-        "https://www.bol.com/nl/nl/menu/categories/subMenu/4",  #Speelgoed, Hobby en Feest
-        "https://www.bol.com/nl/nl/menu/categories/subMenu/5",  #Zwanger, Baby & Peuter
-        "https://www.bol.com/nl/nl/menu/categories/subMenu/6",  #Mooi & Gezond
-        "https://www.bol.com/nl/nl/menu/categories/subMenu/7",  #Kleding, Schoenen & Accessoires
-        "https://www.bol.com/nl/nl/menu/categories/subMenu/8",  #Sport, Outdoor & Reizen
-        "https://www.bol.com/nl/nl/menu/categories/subMenu/12", #Kantoor & School
-        "https://www.bol.com/nl/nl/menu/categories/subMenu/11", #Wonen, Koken & Huishouden
-        "https://www.bol.com/nl/nl/menu/categories/subMenu/9",  #Dier, Tuin & Klussen
-        "https://www.bol.com/nl/nl/menu/categories/subMenu/13"  #Auto & Motor
+        #"https://www.bol.com/nl/nl/menu/categories/subMenu/3",  #Computer en Elekronica
+        #"https://www.bol.com/nl/nl/menu/categories/subMenu/4",  #Speelgoed, Hobby en Feest
+        #"https://www.bol.com/nl/nl/menu/categories/subMenu/5",  #Zwanger, Baby & Peuter
+        #"https://www.bol.com/nl/nl/menu/categories/subMenu/6",  #Mooi & Gezond
+        "https://www.bol.com/nl/nl/menu/categories/subMenu/7"  #Kleding, Schoenen & Accessoires
+        #"https://www.bol.com/nl/nl/menu/categories/subMenu/8",  #Sport, Outdoor & Reizen
+        #"https://www.bol.com/nl/nl/menu/categories/subMenu/12", #Kantoor & School
+        #"https://www.bol.com/nl/nl/menu/categories/subMenu/11", #Wonen, Koken & Huishouden
+        #"https://www.bol.com/nl/nl/menu/categories/subMenu/9",  #Dier, Tuin & Klussen
+        #"https://www.bol.com/nl/nl/menu/categories/subMenu/13"  #Auto & Motor
     ]
     
     #TODO: MAKE SEPARATE FUNCTION
@@ -172,4 +185,6 @@ if __name__ == '__main__':
             if "alles" in title.lower() or "alle" in title.lower():
                 pass
             else:
-                pass
+                #print(title)
+                #print(main_sub_cat.get_attribute("href"))
+                find_lowest_categories(main_sub_cat.get_attribute("href"))
