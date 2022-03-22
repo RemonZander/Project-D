231bb1f5096d926b01209fad02364eef0ef052d2
Sebastiaan Booman <iMeowTwitch@gmail.com>
2022-03-22 19:22:47 +0100

    Fixed jeans scraping

--- Server/Webscraper/test2.py
+++ Server/Webscraper/test2.py
@@ -3,56 +3,63 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-import requests
 
 def scrape_page_layout_one(n):
     #GET LIST OF ALL ITEMS
     items = driver.find_elements(By.CSS_SELECTOR, ".js_item_root:not(.js_sponsored_product)")
-    #print(f"length: {len(items)}")
-    #print(f"page: {n}")
+    print(f"length: {len(items)}")
+    print(f"page: {n}")
+    image_one = driver.find_element(By.CSS_SELECTOR, ".js_item_root:not(.js_sponsored_product) > .product-item__image")
+    #for attr in range(len(image_one.get_property("attributes"))):
+                   # print(f"INDEX: {attr},\n {image_one.get_property('attributes')[attr]}\n\n")
+    #print(image_one)
+
     for item in items:
         #RETRIEVE NAME
         #name = item.find_element(By.CSS_SELECTOR, ".product-item__content > .product-item__info > .product-title--inline").text
         #TRY-BLOCK FOR INCONSISTENCY PRODUCT IMAGES PLACEMENT IN WEBELEMENT
         try:
-            image_link = item.find_element(By.CSS_SELECTOR, ".product-item__image > div > a > img").get_attribute("src")
-        except:
-            image_link = item.find_element(By.CSS_SELECTOR, ".product-item__image > div > a > .skeleton-image > div > img")
+            image_link = item.find_element(By.CSS_SELECTOR, ".product-item__image > a > img").get_attribute("src")
+        except Exception as e:
+            print(e)
+            print("Except")
+            image_link = item.find_element(By.CSS_SELECTOR, ".product-item__image > a > .skeleton-image > .skeleton-image__container > img")
             if image_link.get_attribute("src") is None:
-               # print("CANNOT FIND SRC IN ELEMENT, TRYING ALTERNATIVE")
-                #for attr in range(len(image_link.get_property("attributes"))):
-                    #print(f"INDEX: {attr},\n {image_link.get_property('attributes')[attr]}\n\n")
+                print("CANNOT FIND SRC IN ELEMENT, TRYING ALTERNATIVE")
+                for attr in range(len(image_link.get_property("attributes"))):
+                    print(f"INDEX: {attr},\n {image_link.get_property('attributes')[attr]}\n\n")
                 image_link = image_link.get_property("attributes")[1]["value"]
-                #print("FOUND IMAGE LINK" + image_link)
-            else:
-                image_link = image_link.get_attribute("src")
+                print("FOUND IMAGE LINK" + image_link)
 
         #TODO: BOTH LINK AND NAME CAN BE FOUND IN SAME CLASSES, CAN IMPROVE THIS FOR CLARITY
         #product_link = item.find_element(By.CSS_SELECTOR, ".product-item__content > .product-item__info > .product-title--inline > a").get_attribute("href")
-        print(f"IMAGE: {image_link}\n")
-        with open (f"{n}.jpg", "wb") as file:
-            r = requests.get(image_link, allow_redirects=True)
-            file.write(r.content)
-        n = n + 1
+        #print(f"NAME: {name}\nIMAGE: {image_link}\nPRODUCT: {product_link}\n")
+            #print("IMAGE: " + image_link)
 
 def iterate_lowest_category(start_url):
     #SET MAXIMUM OF PAGES TO LOAD (EACH PAGE HAS MAXIMUM OF 24 PRODUCTS)
-    for n in range(1,199):
+    for n in range(1,200):
         driver.get(start_url + f"?page={n}")
         #TRY INCASE PAGE DOES NOT EXIST (OUT OF RANGE), OR PAGE SETUP IS DIFFERENT (CLOTHING FOR EXAMPLE HAS DIFFERENT SETUP)
-        scrape_page_layout_one(n) #TODO: REMOVE N FROM FUNCTION (ONLY NEEDED FOR DEBUGGING)
+        #try:
+            #scrape_page_layout_one(n) #TODO: REMOVE N FROM FUNCTION (ONLY NEEDED FOR DEBUGGING)       
+        scrape_page_layout_one(n)
+        #except:
+            #print("PAGE LAYOUT FAILED")
+            #break
+
+
+
 
 if __name__ == '__main__':
     base_url = "https://www.bol.com"
 
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('headless')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=chrome_options)
-    
     #driver.implicitly_wait(20)
     driver.maximize_window()
     driver.get(base_url)
+
     #ACCEPT COOKIES
     driver.find_element(By.CSS_SELECTOR, "wsp-consent-modal > div > button").click()
     #get_list()
