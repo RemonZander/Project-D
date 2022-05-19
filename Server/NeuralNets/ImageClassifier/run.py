8a1e9ebdbdb7080e9ff7b8eaf12bd3268db5cd10
Quinn Rachman <quinnrachman@gmail.com>
2022-05-19 11:08:09 +0200

    PD-9, PD-15; Fixed project folder

--- Server/NeuralNets/ImageClassifier/run.py
+++ Server/NeuralNets/ImageClassifier/run.py
@@ -6,9 +6,11 @@ img_height = 180
 img_width = 180
 
 img_urls = [
-    ("Jack Jones Poloshirt", "https://media.s-bol.com/3l5kj2WvkQZM/157x210.jpg"),
-    ("Jack & Jones Glenn Slim Ft Heren Jeans", "https://media.s-bol.com/JZmO141NrDoD/71x210.jpg"),
-    ("Teva W Dames Sandalen", "https://media.s-bol.com/xlR1l6EjjJl3/168x78.jpg"),
+    ("Regular fit stretch jeans, straight", "https://image01.bonprix.nl/assets/460x644/1642409133/19339429-3PcU60Wk.jpg"),
+    ("PETROL T-SHIRT", "https://cdn.shopify.com/s/files/1/0103/3987/6928/products/aurelien_t-shirt_men_egyptian_cotton_heren_katoen_petrol_49db354a-1f9b-49f7-b22e-0491e5d95f46_600x.jpg?v=1634569755"),
+    ("Nike Air Force 1 '07", "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/350e7f3a-979a-402b-9396-a8a998dd76ab/air-force-1-07-herenschoen-pXTXQ8.png"),
+    ("PME Legend Falcon Navy", "https://www.fashionforless.nl/media/catalog/product/cache/ad39a5933e487132b4cc9b7a888cdc49/p/b/pbo202041-598_1.jpg"),
+    ("heren teenslippers zwart", "https://www.hema.nl/dw/image/v2/BBRK_PRD/on/demandware.static/-/Sites-HEMA-master-catalog/default/dw9529b877/product/22170701_01_001_01.jpg?sw=1058&sh=1200&sm=fit"),
 ]
 
 batches = [
@@ -31,7 +33,7 @@ for index, url in enumerate(img_urls):
     batch = tf.expand_dims(img_array, 0)
     batches.append((url[0], batch))
 
-model = tf.keras.models.load_model(os.path.abspath('./Server/NeuralNets/ImageClassifier/model.h5'))
+model = tf.keras.models.load_model("ImageClassifier/model.h5")
 
 print("==================================================Start Prediction=================================================")
 for batch in batches:
