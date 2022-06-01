3ec9416767c7c78d83dfb3a712844aaefd146d0c
NielsPoldervaart <Niels@meghira.nl>
2022-06-01 14:35:27 +0200

    PD-2, PD-29; Changed if statement to guard clause

--- WebExtension/searchImage.js
+++ WebExtension/searchImage.js
@@ -8,39 +8,41 @@ chrome.contextMenus.create({
 // Register a listener for the `onClicked` event
 chrome.contextMenus.onClicked.addListener((clickedData, tab) => {
 	if (
-		tab &&
-		clickedData.mediaType === "image" &&
-		clickedData.menuItemId === "bolImageSearch"
-	) {
-		// Fetch request to get the image from the image src url
-		fetch(clickedData.srcUrl).then(async (res) => {
-			// Convert the image to a blob
-			const contentType = res.headers.get("content-type");
-			const blob = await res.blob();
+		!tab ||
+		clickedData.mediaType !== "image" ||
+		clickedData.menuItemId !== "bolImageSearch"
+	)
+		return;
 
-			// Making the blob into an file object
-			const imgFile = new File([blob], "image.jpg", { contentType });
+	// Fetch request to get the image from the image src url
+	fetch(clickedData.srcUrl).then(async (res) => {
+		// Convert the image to a blob
+		const contentType = res.headers.get("content-type");
+		const blob = await res.blob();
 
-			// Adding the imgFile to the formData with the key "image" for the post request
-			const data = new FormData();
-			data.append("image", imgFile);
+		// Making the blob into an file object
+		const imgFile = new File([blob], "image.jpg", { contentType });
 
-			// Post request to the server sending the formdata as the data.
-			fetch(`http://127.0.0.1:5000/image`, {
-				method: "POST",
-				body: data,
+		// Adding the imgFile to the formData with the key "image" for the post request
+		const data = new FormData();
+		data.append("image", imgFile);
+
+		// Post request to the server sending the formdata as the data.
+		fetch(`http://127.0.0.1:5000/image`, {
+			method: "POST",
+			body: data,
+		})
+			.then((res) => {
+				res.json();
+				console.log(res);
 			})
-				.then((res) => {
-					res.json();
-					console.log(res);
-				})
-				.catch((error) =>
-					console.log("Authorization failed : " + error.message)
-				);
-		});
-	}
+			.catch((error) =>
+				console.log("Authorization failed : " + error.message)
+			);
+	});
 });
 
+// Function that runs the createOverlay.js file when the extension icon is clicked
 chrome.action.onClicked.addListener((tab) => {
 	chrome.scripting.executeScript({
 		target: { tabId: tab.id },
