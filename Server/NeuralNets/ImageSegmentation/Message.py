d9ea9f52d5c74b0f0ec95c0aacc01e50a0dadcdd
Sebastiaan Booman <iMeowTwitch@gmail.com>
2022-06-16 12:59:21 +0200

    PD-13, PD-19, Added basic TCP servers for nn

---
+++ Server/NeuralNets/ImageSegmentation/Message.py
@@ -0,0 +1,29 @@
+import json
+
+class Message():
+    def __init__(self, user_index: int, content, complex_case: bool):
+        self.user_index = user_index
+        self.content = content
+        self.complex_case = complex_case
+
+    def __iter__(self):
+        yield from {
+            "user_index" : self.user_index,
+            "content": self.content,
+            "complex_case": self.complex_case
+        }.items()
+
+    def __str__(self):
+        return json.dumps(dict(self), ensure_ascii=False)
+        
+    def __repr__(self):
+        return self.__str__()
+
+    def to_json(self):
+        return self.__str__()
+
+    @staticmethod
+    def from_json(json_dict):
+        return Message(json_dict['user_index'], json_dict['content'], json_dict['complex_case'])
+
+#https://levelup.gitconnected.com/how-to-deserialize-json-to-custom-class-objects-in-python-d8b92949cd3b
